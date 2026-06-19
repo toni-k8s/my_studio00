71fa45d7db9f4922d592ea198ee97a564b7ae0df
guan xing <cris_gx@163.com>
2026-06-19 20:36:46 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,6 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	std::min(1, 2);
 	std::max(1, 2);
-	return 0;
-}#include <iostream>
-#include <algorithm>
-#include <iostream>
-
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	std::min(1, 2);
-	std::max(1, 2);
-	return 0;
-}#include <iostream>
-#include <algorithm>
-#include <iostream>
-
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	std::min(1, 2);
-	std::max(1, 2);
-	return 0;
-}#include <iostream>
-#include <algorithm>
-#include <iostream>
-
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	std::min(1, 2);
-	std::max(1, 2);
+	//20:36
 	return 0;
 }
